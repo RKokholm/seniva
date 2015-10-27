5049d6bf7779bd4b6178630d3ceeae079c6c2cd8
Rasmus <KokholmGames@hotmail.com>
2015-10-27 23:15:19 +0100

    added order choices

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -6,24 +6,42 @@
 
     @include('partials.nav')
 
-    <div class="container_slick">
+    <div class="container_medium">
 
 	    <div class="row">
 
 	        <div class="title">Vælg din bestillingstype</div>
-			
-			<div class="col-6">
-				
-				<div class="item"></div>
 
-			</div>
-			<div class="col-6">
+			<div class="col-4">
 				
 				<div class="item">
 					
-					<div class="item_title">Byg selv</div>
+					<div class="category_title">Enkelt produkt</div>
 
-					<i class="fa fa-cog"></i>
+					<i class="fa fa-file-image-o"></i>
+
+				</div>
+
+			</div>
+			
+			<div class="col-4">
+				
+				<div class="item">
+					
+					<div class="category_title">Færdige pakker</div>
+
+					<i class="fa fa-archive"></i>
+
+				</div>
+
+			</div>
+			<div class="col-4">
+				
+				<div class="item">
+					
+					<div class="category_title">Byg selv</div>
+
+					<i class="fa fa-cogs"></i>
 
 				</div>
 
